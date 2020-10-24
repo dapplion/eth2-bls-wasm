94abfcff13ec6b317c982a6538b55f6be6601d9a
dapplion <guillem.lion.benosa@gmail.com>
2020-10-24 17:18:40 +0200

    Use a placeholder bls-eth-wasm repo

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 # exit when any command fails
 set -e
 
-export TARGET_DIR=bls-eth-wasm-dist
-export DIST_DIR=dist
+export TARGET_DIR=bls-eth-wasm-dist/
+export DIST_DIR=dist/
+
+mkdir -p $TARGET_DIR
+mkdir -p $DIST_DIR
+
+# Step 0: Copy to bls-eth-wasm to prevent causing git differences
+cp -r bls-eth-wasm-upstream bls-eth-wasm
 
 # Step 1: Build herumi's C lib with emcc
 rm -f bls-eth-wasm/bls_c.js && rm -f bls-eth-wasm/bls_c.wasm
@@ -12,13 +18,12 @@ cp bls-eth-wasm/bls_c.wasm $TARGET_DIR
 
 # Step 2: Apply custom code as a diff to herumi's JS wrapper in herumi/bls-eth-wasm
 yarn patch-package
-mkdir -p $TARGET_DIR
 cp node_modules/@herumi/bls-eth-wasm/bls.js $TARGET_DIR
 cp node_modules/@herumi/bls-eth-wasm/test.js $TARGET_DIR
 
 # Step 3: Inline WASM code to bls.js for easy distribution of the package
 rm -rf dist
-babel $TARGET_DIR -d $DIST_DIR --copy-files
+yarn babel $TARGET_DIR -d $DIST_DIR --copy-files
 
 # Step 4: Test
 node $DIST_DIR/test
