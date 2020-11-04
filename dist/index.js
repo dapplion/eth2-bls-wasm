80b6fe042835384cfda66a2f73c78fec0df37e77
dapplion <guillem.lion.benosa@gmail.com>
2020-11-04 12:24:22 +0000

    publish built files

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto')
+const createBlsModule = require(`./bls_c.js`)
+const blsSetupFactory = require('./bls')
+
+const getRandomValues = crypto.randomFillSync
+const bls = blsSetupFactory(createBlsModule, getRandomValues)
+
+module.exports = bls
