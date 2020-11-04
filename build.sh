4697e6d4f75555c43673fb941d6de3bdf80dc5ee
dapplion <guillem.lion.benosa@gmail.com>
2020-11-04 10:46:38 +0000

    Publish dist directory to NPM

--- build.sh
+++ build.sh
@@ -5,14 +5,6 @@ set -e
 # $ ./emsdk/emsdk activate
 # source "./emsdk/emsdk_env.sh"
 
-export LIB_DIR=lib/
-export DIST_DIR=dist/
-
-rm -rf $LIB_DIR
-rm -rf $DIST_DIR
-mkdir -p $LIB_DIR
-mkdir -p $DIST_DIR
-
 # Copy to bls-eth-wasm to prevent causing git differences
 rm -rf bls-eth-wasm
 cp -r -T bls-eth-wasm-upstream bls-eth-wasm
@@ -20,7 +12,9 @@ cp -r -T bls-eth-wasm-upstream bls-eth-wasm
 # Build herumi's C lib with emcc
 rm -f bls-eth-wasm/bls_c.js && rm -f bls-eth-wasm/bls_c.wasm
 cd bls && make clean && make bls-eth-wasm && cd ..
-cp bls-eth-wasm/bls_c.js $LIB_DIR
 
 # Add types
 cp types/* bls-eth-wasm/
+
+# Rename dir to dist for publishing to NPM
+cp -r -T bls-eth-wasm/ dist/
